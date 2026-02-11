c1775cf3d4e40cc0fac3d291d16718429ab75924
raulmpsousa <raulmpsousa@gmail.com>
2026-02-11 19:19:03 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import process from 'process';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY)
-    }
-  }
+export default defineConfig({
+  plugins: [react()],
 })
